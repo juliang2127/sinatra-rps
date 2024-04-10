9ce2ab77e85fc11827dcad92775730437484e973
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-04-10 23:03:27 +0000

    RPS done

--- app.rb
+++ app.rb
@@ -1,64 +1,52 @@
-#require "sinatra"
-#require "sinatra/reloader"
+require "sinatra"
+require "sinatra/reloader"
 
-#get("/") do
+get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
-#end
+  erb(:elephant)
+end
 
-comp_array = ["rock", "paper", "scissors"] 
-my_array = ["rock", "paper", "scissors"] 
-comp_array_sample = comp_array.sample
-my_array_sample = my_array.sample
+get("/rock") do 
+  comp_array = ["rock", "paper", "scissors"]
+  @comp_array_sample = comp_array.sample
 
-pp comp_array_sample = comp_array.sample
-pp my_array_sample = my_array.sample
+  if @comp_array_sample == "rock"
+      @outcome = "tied"
+  elsif @comp_array_sample == "scissors" 
+      @outcome = "won"
+  elsif @comp_array_sample == "paper" 
+      @outcome = "lost"
+  end
+  erb(:rock)
+end
 
-if 
-  comp_array_sample == "rock" && my_array_sample == "rock"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"
-    pp "You Tied!"
-elsif 
-  comp_array_sample == "paper" && my_array_sample == "paper"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"  
-    pp "You Tied!"
-elsif
-  comp_array_sample == "scissors" && my_array_sample == "scissors"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"  
-    pp "You Tied!"
-elsif
-  comp_array_sample == "rock" && my_array_sample == "scissors"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"
-    pp "You Lost!"
-elsif
-  comp_array_sample == "scissors" && my_array_sample == "paper"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"
-    pp "You Lost!"
-elsif
-  comp_array_sample == "paper" && my_array_sample == "rock"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"
-    pp "You Lost!"
-elsif
-  comp_array_sample == "paper" && my_array_sample == "scissors"
-    pp "We played #{my_array_sample}!"
-    pp "They Played #{comp_array_sample}"
-    pp "You Won!"
-elsif
-  comp_array_sample == "scissors" && my_array_sample == "rock"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"
-    pp "You Won!"
-elsif
-  comp_array_sample == "rock" && my_array_sample == "paper"
-    pp "We played #{my_array_sample}!"
-    pp "They played #{comp_array_sample}"
-    pp "You Won!"
+get("/paper") do 
+  comp_array = ["rock", "paper", "scissors"]
+  @comp_array_sample = comp_array.sample
+  
+  if @comp_array_sample == "paper"
+      @outcome = "tied"
+  elsif @comp_array_sample == "rock" 
+      @outcome = "won"
+  elsif @comp_array_sample == "scissors" 
+      @outcome = "lost"
+  end
+  erb(:paper)
+end
+
+get("/scissors") do 
+  comp_array = ["rock", "paper", "scissors"]
+  @comp_array_sample = comp_array.sample
+  
+  if @comp_array_sample == "scissors"
+      @outcome = "tied"
+  elsif @comp_array_sample == "paper" 
+      @outcome = "won"
+  elsif @comp_array_sample == "rock" 
+      @outcome = "lost"
+  end
+  erb(:scissors)
 end
